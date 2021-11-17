904b8f22d0f80fe62fdb0af2b145f18c0b07cd0a
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2021-11-17 18:35:48 +0000

    Fixing navbar toggler issue

--- portfolio/src/components/Header.js
+++ portfolio/src/components/Header.js
@@ -14,8 +14,12 @@ function Header() {
                 <NavbarBrand href="/">
                     Saadia El fekak
                 </NavbarBrand>
-                <NavbarToggler onClick={function noRefCheck() { }} />
-                <Collapse navbar>
+                <button className="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+                    data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
+                    aria-label="Toggle navigation">
+                    <span className="navbar-toggler-icon"></span>
+                </button>
+                <Collapse navbar id="navbarResponsive">
                     <Nav
                         className="me-auto"
                         navbar
