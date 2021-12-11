ee2867409dd08431107996124ae726a528e26f8c
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2021-12-11 20:15:37 +0000

    Adding routes

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Masthead from "./components/Masthead";
+import AboutMe from "./pages/AboutMe";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact";
 
+// Calling all components and using router to render pages
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <div>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<Masthead />} />
+          <Route exact path="/about" element={<AboutMe />} />
+          <Route exact path="/portfolio" element={<Portfolio />} />
+          <Route exact path="/contact" element={<Contact />} />
+        </Routes>
+        <Footer />
+      </div>
+    </BrowserRouter>
+  )
+
 }
 
 export default App;
