1d51b0b92e17d3dc3c4fa4f1e9b7fdf4e1b6d631
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2021-12-15 00:35:54 +0000

    Fixing deployment issues

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {
-  BrowserRouter,
+  HashRouter,
   Routes,
   Route
 } from "react-router-dom";
@@ -14,7 +14,7 @@ import Contact from "./pages/Contact";
 // Calling all components and using router to render pages
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename="/">
       <div>
         <Header />
         <Routes>
@@ -25,7 +25,7 @@ function App() {
         </Routes>
         <Footer />
       </div>
-    </BrowserRouter>
+    </HashRouter>
   )
 
 }
