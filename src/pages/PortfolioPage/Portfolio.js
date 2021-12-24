2cf939425aaa5e755d98d994d3aa0d54e2cdd3d2
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2021-12-24 23:26:11 +0000

    Adding style by page

---
+++ src/pages/PortfolioPage/Portfolio.js
@@ -0,0 +1,46 @@
+import React from "react";
+import ProjectsList from "../../Components/ProjectsList/ProjectsList";
+import { Container, Row } from "reactstrap";
+// importing projects info from js file
+import { projects } from "../../Seeds/Projects";
+import "./Portfolio.css";
+
+function Portfolio() {
+
+    return (
+        <section className="portfolio-section" id="portfolio">
+            <Container>
+                <Row className="align-items-center no-gutters mb-4 mt-4 mb-lg-5">
+                    <h1 className="text-center">My portfolio</h1>
+                </Row>
+                <div id="projectsCarousel" className="carousel slide carousel-fade" data-ride="carousel">
+                    <div className="carousel-inner">
+                        {/* Mapping through the projects array and creating a component for each project */}
+                        {projects.map(project => (
+                            <ProjectsList
+                                key={project.id}
+                                title={project.title}
+                                image={project.image}
+                                description={project.description}
+                                github={project.github}
+                                demo={project.demo}
+                            />
+                        ))}
+                        {/* Previous and Next carousel buttons */}
+                        <button className="carousel-control-prev" type="button" data-target="#projectsCarousel" data-slide="prev">
+                            <span className="carousel-control-prev-icon bg-dark bg-opacity-50" aria-hidden="true"></span>
+                            <span className="visually-hidden">Previous</span>
+                        </button>
+                        <button className="carousel-control-next" type="button" data-target="#projectsCarousel" data-slide="next">
+                            <span className="carousel-control-next-icon bg-dark bg-opacity-50" aria-hidden="true"></span>
+                            <span className="visually-hidden">Next</span>
+                        </button>
+                    </div>
+                </div>
+            </Container>
+        </section>
+    );
+}
+
+
+export default Portfolio;
