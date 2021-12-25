482cc7e212d708e0932398e5422a33efb455df90
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2021-12-25 19:29:05 +0000

    "reverting commit"

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Footer.css";
+
+// Footer component
+function Footer() {
+    return (
+            <footer className="footer fixed-bottom">
+                <p className="text-center text-muted "> © 2021 All Rights Reserved, by Saadia El fekak </p>
+            </footer>
+    )
+}
+
+export default Footer;
