8ce6fe4dec1ded0d8d0587393ea9c2a8b0d821f5
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2021-12-25 02:10:24 +0000

    Fixing responsiveness issues

--- src/pages/PortfolioPage/Portfolio.js
+++ src/pages/PortfolioPage/Portfolio.js
@@ -13,6 +13,7 @@ function Portfolio() {
                 <Row className="align-items-center no-gutters mb-4 mt-4 mb-lg-5">
                     <h1 className="text-center">My portfolio</h1>
                 </Row>
+                <Row className="align-items-center no-gutters mx-auto">
                 <div id="projectsCarousel" className="carousel slide carousel-fade" data-ride="carousel">
                     <div className="carousel-inner">
                         {/* Mapping through the projects array and creating a component for each project */}
@@ -27,16 +28,17 @@ function Portfolio() {
                             />
                         ))}
                         {/* Previous and Next carousel buttons */}
-                        <button className="carousel-control-prev" type="button" data-target="#projectsCarousel" data-slide="prev">
+                        <button className="carousel-control-prev btn-control" type="button" data-target="#projectsCarousel" data-slide="prev">
                             <span className="carousel-control-prev-icon bg-dark bg-opacity-50" aria-hidden="true"></span>
                             <span className="visually-hidden">Previous</span>
                         </button>
-                        <button className="carousel-control-next" type="button" data-target="#projectsCarousel" data-slide="next">
+                        <button className="carousel-control-next btn-control" type="button" data-target="#projectsCarousel" data-slide="next">
                             <span className="carousel-control-next-icon bg-dark bg-opacity-50" aria-hidden="true"></span>
                             <span className="visually-hidden">Next</span>
                         </button>
                     </div>
                 </div>
+                </Row>
             </Container>
         </section>
     );
