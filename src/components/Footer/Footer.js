5cc7bf5b3d56ef8bf4cc78c9d1049231dddbd046
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2021-12-25 02:06:00 +0000

    Changing color and adding shadows

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 // Footer component
 function Footer() {
     return (
-            <footer className="footer mt-auto fixed-bottom">
+            <footer className="footer fixed-bottom">
                 <p className="text-center text-muted "> © 2021 All Rights Reserved, by Saadia El fekak </p>
             </footer>
     )
