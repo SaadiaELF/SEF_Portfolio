e1f98141740ac390c73376fc6910125573cf5935
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2022-01-27 00:18:32 +0000

    Restyling the component

--- src/Components/ContactCard/ContactCard.js
+++ src/Components/ContactCard/ContactCard.js
@@ -8,7 +8,7 @@ function ContactCard(props) {
         <Col md="3">
             <Card id="contactCard">
                 <CardBody className="text-center">
-                    <i className={`fas fa-${props.icon} text-black mb-2 fa-2x`}></i>
+                    <i className={`fas fa-${props.icon} fa-2x`} aria-hidden="true"></i>
                     <h4 className="text-uppercase m-0">{props.title}</h4>
                     <hr />
                     <div className="text-black-50">{props.text}</div>
