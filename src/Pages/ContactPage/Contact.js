d7141d02f0d04551d4a66d024943370d9e61095d
SaadiaELF <74792703+SaadiaELF@users.noreply.github.com>
2022-01-27 00:19:57 +0000

    Adding PopupButton from react-calendly

--- src/Pages/ContactPage/Contact.js
+++ src/Pages/ContactPage/Contact.js
@@ -1,18 +1,18 @@
 import React from "react";
 import { Container, Row } from "reactstrap";
+import { PopupButton } from "react-calendly";
 import "./Contact.css";
 import ContactCard from "../../Components/ContactCard/ContactCard"
-
 
 function Contact() {
     return (
         <div>
             <section className="contact-section" id="contact">
-                <Container>
+                <Container >
                     <Row className="align-items-center no-gutters mb-4 mt-4 mb-lg-5">
                         <h1 className="text-center">Contact me</h1>
                     </Row>
-                    <Row>
+                    <Row className="contact-container">
                         <ContactCard
                             icon="map-marked-alt"
                             title="Address"
@@ -32,6 +32,11 @@ function Contact() {
                             icon="file"
                             title="Resume"
                             text={<a href="https://drive.google.com/file/d/1r9wMMGY2XgtQHHKb7RdbjsptDptmgMQ9/view?usp=sharing" target="_blank" rel="noreferrer">My resume</a>}
+                        />
+                        <ContactCard
+                            icon="calendar-alt"
+                            title="Calendly"
+                            text={<PopupButton className="btn btn-primary" url="https://calendly.com/saadiaelfekak/30min" text="Schedule a meeting" styles={{width:"100%"}} />}
                         />
                     </Row>
                 </Container>
